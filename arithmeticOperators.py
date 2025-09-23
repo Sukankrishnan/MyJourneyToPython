d5a49fa01bdd08b422b2ff1e366b85ac56321d09
Sukankrishnan <sukan.santhanam@gmail.com>
2025-09-23 18:37:33 +0200

    Adding files

---
+++ arithmeticOperators.py
@@ -0,0 +1,15 @@
+# print 10 to the power of 3 - ** is used for exponential
+print (10 ** 3)
+
+#augmented assignment operator += -= *= /= %=
+num=30
+num = num*2
+num *= 3
+print (num)
+
+#comparison operators == <= >= < > !=
+#logical operators - and or not
+price=10
+print (price>5 and price<100)
+print(price>30 or price<100)
+print(not price>2)
---
+++ convertBodyWeight.py
@@ -0,0 +1,14 @@
+#convert KG to LBS or LBS to KG
+
+weight=float(input("Enter your weight"))
+weightUnit=input("Is this weight in (K)g or (L)bs:")
+
+if weightUnit.upper() == "K":
+    weightInLBS = weight*2.20462
+    print("Your weight in LBS is:",weightInLBS)
+elif weightUnit.upper() == "L":
+    weightInKG = weight*0.4535
+    print("Your weight in KG is:",weightInKG)
+
+#print(weightUnit.upper())
+
