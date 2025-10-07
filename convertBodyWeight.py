c474ed38d1aee7e3b80bc0d596039044c577fc58
Sukankrishnan <sukan.santhanam@gmail.com>
2025-10-07 18:25:59 +0200

    Add files

--- convertBodyWeight.py
+++ convertBodyWeight.py
@@ -4,8 +4,8 @@ weight=float(input("Enter your weight"))
 weightUnit=input("Is this weight in (K)g or (L)bs:")
 
 if weightUnit.upper() == "K":
-    weightInLBS = weight*2.20462
-    print("Your weight in LBS is:",weightInLBS)
+    weightInLBS = weight / 0.45
+    print("Your weight in LBS is:"+ str(weightInLBS))
 elif weightUnit.upper() == "L":
     weightInKG = weight*0.4535
     print("Your weight in KG is:",weightInKG)
